27b23742d395fd010415adc1b9e21411ec6b034f
flyingulysses <wangyong_coder@qq.com>
2018-04-08 17:06:05 +0800

    spring boot + mybatis +oracle +freemark

---
+++ springBoot/FlightMonitor/src/main/java/com/siniswift/flightMonitor/FlightMonitorApplication.java
@@ -0,0 +1,12 @@
+package com.siniswift.flightMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightMonitorApplication.class, args);
+	}
+}
