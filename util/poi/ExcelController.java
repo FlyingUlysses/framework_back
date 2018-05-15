704aa5f09b6e2d24cdcf7852c61f4a13c8de4185
flyingulysses <wangyong_coder@qq.com>
2018-05-15 10:14:59 +0800

    apache poi 3.2.6 wait sometime update to 3.2.7

---
+++ util/poi/ExcelController.java
@@ -0,0 +1,85 @@
+package com.siniswift.flightMonitor.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.siniswift.flightMonitor.entity.FlightEntity;
+import com.siniswift.flightMonitor.pojo.ExcelData;
+import com.siniswift.flightMonitor.pojo.LoginConfig;
+import com.siniswift.flightMonitor.service.FlightService;
+import com.siniswift.flightMonitor.utils.Constants;
+import com.siniswift.flightMonitor.utils.ExportExcelUtils;
+
+@RestController
+public class ExcelController {
+	
+	@Autowired
+	private FlightService flightService;
+	
+	@RequestMapping(value = "/excel/export", method = RequestMethod.GET)
+    public void excel(HttpSession session,HttpServletResponse response,String start_time,String end_time,String type,String is_start) throws Exception {
+		ArrayList<ExcelData> dataList = new ArrayList<ExcelData>();
+        ExcelData data = new ExcelData();
+        data.setName("航班监控");
+        List<String> titles = new ArrayList<>();
+        titles.add("航班名称");
+        titles.add("起飞机场");
+        titles.add("落地机场");
+        titles.add("预计起飞时间");
+        titles.add("预计到达时间");
+        data.setTitles(titles);
+        LoginConfig config  = (LoginConfig) session.getAttribute(Constants.LOGIN_CONFIG);
+        SimpleDateFormat format = new SimpleDateFormat("YYYY-MM-dd HH:mm");
+        HashMap<String, ArrayList<FlightEntity>> map = flightService.reloadExcelTable(type,is_start, start_time, end_time, config.getSeatId());
+        List<List<Object>>rows = new ArrayList<>();
+        ArrayList<FlightEntity> flightList = new ArrayList<>();
+        if(map.get("started") !=null) {
+        	flightList.addAll(map.get("started"));
+        }
+        if(map.get("wait") != null) {
+        	flightList.addAll(map.get("wait"));
+        }
+        for (FlightEntity flight : flightList) {
+        	List<Object> row = new ArrayList<>();
+        	row.add(flight.getFlightNumber());
+        	row.add(flight.getDepCityName());
+        	row.add(flight.getArrCityName());
+        	row.add(format.format( flight.getStd() ));
+        	row.add(format.format( flight.getSta() ));
+        	rows.add(row);
+		}
+
+        data.setRows(rows);
+        //生成本地
+        /*File f = new File("c:/test.xlsx");
+        FileOutputStream out = new FileOutputStream(f);
+        ExportExcelUtils.exportExcel(data, out);
+        out.close();*/
+        dataList.add(data);
+        ExcelData data2 = new ExcelData();
+        data2.setName("测试测试");
+        data2.setTitles(titles);
+        data2.setRows(rows);
+        dataList.add(data2);
+        ExportExcelUtils.exportExcel(response,"航班情况.xlsx",dataList);
+    }
+	
+	@RequestMapping(value="/excel/reloadTable")
+	@ResponseBody
+	public HashMap<String, ArrayList<FlightEntity>> reloadTable(HttpSession session,String start_time,String end_time,String type,String is_start) {
+		LoginConfig config  = (LoginConfig) session.getAttribute(Constants.LOGIN_CONFIG);
+		return flightService.reloadExcelTable(type,is_start, start_time, end_time, config.getSeatId());
+	}
+	
+}
